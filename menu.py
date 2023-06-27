87276171a7cfd0ad8313a7cb841aa941f687c687
Lilian G <97319339+gimenezt@users.noreply.github.com>
2023-06-27 06:55:19 -0300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,4 +1,4 @@
-from Functions import select, insert, update, delete
+from functions import select, insert, update, delete
 
 def main():
     var = int(input('\nBem-vindo(a)! \n\nO que deseja fazer?\n\n1 - Visualizar informações\n2 - Adicionar\n3 - Editar\n4 - Deletar\n'))
